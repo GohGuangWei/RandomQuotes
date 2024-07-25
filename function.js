1b919bf3490e2d972d8c00c5fc974592a24f42ed
Goh Guang Wei <darrengohguangwei1@gmail.com>
2024-07-26 00:30:47 +0800

    Pressing refresh button would randomize the quotes

--- function.js
+++ function.js
@@ -1,27 +1,48 @@
-//It defines a constant reference to an array
-const quotes = [];
+// //fs = file server
+// function readFile()
+// {
+//   const fs = require("fs")
+//   let contents = fs.readFileSync("quotes.txt").toString().split(/\r?\n/)
+//   let size = contents.length
+//   let x = Math.floor((Math.random() * (size)))
+//   return(contents[x])
+// }
 
-function loadJson()
+// //replaces html text with a random quote
+// function replaceQuote() 
+// {
+//   var test = readFile()
+//   document.getElementById("quotes").innerHTML = test;
+//   document.getElementById("name").innerHTML = "testttt"
+// }
+
+function readFile() 
 {
-  fetch('./quotes.json')
-    .then((response) => response.json())
-    .then((data) => replaceQuote(data));
+  //Requests quotes.txt
+  return fetch('quotes.txt')
+    //When the request returns a response.
+    .then(response => response.text()) //Resolves with a string
+    .then(data => {
+      const contents = data.split(/\r?\n/); //Splits text and stores into array
+      const size = contents.length;  //Determines sizes of array
+      const x = Math.floor(Math.random() * size); //Gets random quote based on size
+
+      const object = contents[x].split(",");
+      const quote = new Quote(object[0],object[1]);
+      return quote;
+    });
 }
 
-
-//replaces html text with a random quote
 function replaceQuote() 
 {
-    loadJson();
-    document.getElementById("quotes").innerHTML = "test";
-    document.getElementById("name").innerHTML = "Name changed.";
-}
-
-//Grabs a random object from the array
-function randomQuote()
-{
-  
-
+  readFile() 
+    .then(quote => {
+      document.getElementById('quotes').innerHTML = quote.quote;
+      document.getElementById('name').innerHTML = quote.name;
+    })
+    .catch(error => {
+      console.error('Error fetching the quote:', error);
+    });
 }
 
 //Object for each quote
@@ -29,15 +50,7 @@ class Quote
 {
   constructor(quote, name)
   {
-    this.quote = quote;
-    this.name = name;
+    this.quote = quote
+    this.name = name
   }
 }
-
-//reads text file and saves them as objects into a vector
-function readFile(file)
-{
-
-}
-
-
